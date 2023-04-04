9d347445af5c276ee11a590dd5eacbcdd520624e
SabrinaY123 <yohannes.sabrina321@gmail.com>
2023-04-04 16:44:57 -0500

    Initial Commit

---
+++ index.js
@@ -0,0 +1,6 @@
+var randomNumber1 = Math.random();
+randomNumber1 = Math.floor(randomNumber1*6) + 1;
+
+document.querySelector(".img1").setAttribute("src", "images/dice3.png");
+
+document.querySelector(".img2").setAttribute("src", "images/dice4.png");
