ec1fa47520b10809fb60bf319f9d1e935796174f
SabrinaY123 <yohannes.sabrina321@gmail.com>
2023-04-05 07:06:55 -0500

    Updated Solution

--- index.js
+++ index.js
@@ -1,46 +1,19 @@
-var randomNumber1 = 6;
+var randomNumber1 = Math.floor(Math.random()*6) + 1;
 
+var randomImageSource1 = "images/dice" + randomNumber1 + ".png";
 
-function generateRandomNumber() {
-    randomNumber1 = Math.random();
-    randomNumber1 = Math.floor(randomNumber1*6) + 1;
+document.querySelectorAll("img")[0].setAttribute("src", randomImageSource1);
 
-    if (randomNumber1 === 1) {
-        document.querySelector(".img1").setAttribute("src", "images/dice1.png");
-        document.querySelector(".img2").setAttribute("src", "images/dice6.png");
-        document.querySelector("h1").innerHTML="Player 2 wins";
-    }
-    
-    if (randomNumber1 === 2) {
-        document.querySelector(".img2").setAttribute("src","images/dice2.png");
-        document.querySelector(".img1").setAttribute("src", "images/dice6.png");
-        document.querySelector("h1").innerHTML="Player 1 wins";
-    }
-    
-    if (randomNumber1 === 3) {
-        document.querySelector(".img1").setAttribute("src", "images/dice3.png");
-        document.querySelector(".img2").setAttribute("src", "images/dice6.png");
-        document.querySelector("h1").innerHTML="Player 2 wins";
-    }
-    
-    if (randomNumber1 === 4) {
-        document.querySelector(".img2").setAttribute("src", "images/dice4.png");
-        document.querySelector(".img1").setAttribute("src", "images/dice6.png");
-        document.querySelector("h1").innerHTML="Player 1 wins";
-    }
-    
-    if (randomNumber1 === 5) {
-        document.querySelector(".img1").setAttribute("src", "images/dice5.png");
-        document.querySelector(".img2").setAttribute("src", "images/dice6.png");
-        document.querySelector("h1").innerHTML="Player 2 wins";
-    }
-    
-    if (randomNumber1 === 6) {
-        document.querySelector(".img2").setAttribute("src", "images/dice1.png")
-        document.querySelector(".img1").setAttribute("src", "images/dice6.png");
-        document.querySelector("h1").innerHTML="Player 1 wins";
-    }
+var randomNumber2 = Math.floor(Math.random()*6) +1;
+
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 1 wins!"
+} else if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 2 wins!"
+} else if (randomNumber1 === randomNumber2) {
+    document.querySelector("h1").innerHTML = "Draw!"
 }
-
-
-generateRandomNumber();
